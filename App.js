1c54868f93876ddb0657401412894fe51837e295
akashkarhana9840 <akarhana004@gmail.com>
2026-07-05 18:33:48 +0530

    ep1 complted

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
-const root=ReactDOM.createRoot(document.getElementById('root'))
-const heading = React.createElement('h1', {}, "hello world from react")
-root.render(heading)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+/*const heading = React.createElement('h1', { id: "heading" }, "hello world from react")
+root.render(heading)*/
+
+const parent = React.createElement("div", { id: 'parent' }, [
+  React.createElement("div", { id: 'child1' }, [
+    React.createElement("h1", {}, "oye mai h1 hun  child 1 ka"),
+    React.createElement("h2", {}, "oye mai h2 hu child 1 ka "),
+  ]),
+  React.createElement("div", { id: 'child2' }, [
+    React.createElement("h1", {}, "oye mai h1 hun  child 2 ka"),
+    React.createElement("h2", {}, "oye mai h2 hu child 2 ka "),
+  ]),
+]);
+root.render(parent);
