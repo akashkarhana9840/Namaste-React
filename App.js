e0e1a79a623076f3f8ce2e9e1ba1a4283af64b3b
akashkarhana9840 <akarhana004@gmail.com>
2026-07-07 13:23:51 +0530

    completed ep 3

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+/*const heading = React.createElement('h1', { id: 'heading' }, 'namaste react')   //core react 
+const root = ReactDOM.createRoot(document.getElementById('root'))  
 
-/*const heading = React.createElement('h1', { id: "heading" }, "hello world from react")
-root.render(heading)*/
-
-const parent = React.createElement("div", { id: 'parent' }, [
-  React.createElement("div", { id: 'child1', key:'child1' }, [
-    React.createElement("h1", {key:"h1-child1"}, "oye mai h1 hun  child 1 ka"),
-    React.createElement("h2", {key:"h2-child1"}, "oye mai h2 hu child 1 ka "),
-  ]),
-  React.createElement("div", { id: 'child2',key:'child2' }, [
-    React.createElement("h1", {key:"h1-child2"}, "oye mai h1 hun  child 2 ka"),
-    React.createElement("h2", {key:"h2-child2"}, "oye mai h2 hu child 2 ka "),
-  ]),
-]);
-root.render(parent);
+const headingjsx = <h1 id='jsxheading'>namaste react in jsx</h1>   //jsx element(this is how we write jsx element)
+root.render(headingjsx);*/
+const root = ReactDOM.createRoot(document.getElementById("root"));//creating root for react
+const Title = () => (<h1>this is my title for component composition</h1>)//functional component
+const elm = 1000 //js variable
+const heading=(<h1>oye mai to js hi hu{elm} </h1>) //element composition
+const ComponentHeading = () => (<div> //functional component
+  <Title />  //component composition
+  <h1>{elm}</h1>   // adding js variable in jsx
+  {heading}        // adding js element in jsx
+  <h1>this is my 1st  component</h1>
+</div>)
+root .render(<ComponentHeading/>) //rendering the component into the root
