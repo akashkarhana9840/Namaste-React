31b5b7311f2cd5b95d9bed758fd04f1eec74af3c
akashkarhana9840 <akarhana004@gmail.com>
2026-07-12 08:15:14 +0530

    live api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,30 @@
 import  RestarauntCard  from "./RestarauntCard";
-import ResList from "../utils/MockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
-    const [Restarauntlist, setRestarauntlist] = useState(ResList);
+  const [Restarauntlist, setRestarauntlist] = useState([]);
+  
+  useEffect(() => { 
+    fetchData()
+  }, []) 
+  const fetchData = async() => {
+     const data=await fetch(
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7041&lng=77.1025&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json();
+    console.log(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants,
+    );
+    setRestarauntlist(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants,
+    );
+
+  }
   return (
     <div className="body">
           <div className="search">
               <button onClick={() => {
                   // Logic for searching 4+ star restaurants
                   setRestarauntlist(Restarauntlist.filter((restaraunt) => restaraunt.info.avgRating > 4.5
-                  ));
+                   ));
               }}>click to search 4+ star Restaurants</button>
       </div>
       <div className=" res-container">
